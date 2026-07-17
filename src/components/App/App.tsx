16b2a159c6b951d4c83f6ec6497e24a23cce8571
Taras <taras.zhovanyk@icloud.com>
2026-07-17 18:51:58 +0200

    using useQuery

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,8 @@
-
 import './App.css'
 import css from './App.module.css'
 import { useState, useEffect } from 'react'
-import { Toaster } from 'react-hot-toast'
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
+import toast, { Toaster } from 'react-hot-toast'
 import SearchBar from '../SearchBar/SearchBar'
 import MovieGrid from '../MovieGrid/MovieGrid'
 import Loader from '../Loader/Loader'
@@ -21,13 +21,22 @@ const ReactPaginate = (
 ).default;
 
 function App() {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [isError, setIsError] = useState<boolean>(false);
-  const [movies, setMovies] = useState<Movie[]>([]);
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
   const [page, setPage] = useState<number>(1);
-  const [totalPages, setTotalPages] = useState<number>(0);
   const [query, setQuery] = useState<string>('');
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['movies', query, page],
+    queryFn: () => fetchMovies(query, page),
+    placeholderData: keepPreviousData,
+    enabled: query.trim() !== '',
+  });
+
+  useEffect(() => {
+    if (data && data.results.length === 0) {
+      toast('No movies found for your request.');
+    }
+  }, [data]);
 
   const handleSelectMovie = (movie: Movie) => {
     setSelectedMovie(movie);
@@ -37,34 +46,13 @@ function App() {
     setSelectedMovie(null);
   };
 
-
-  const handleSearch = (query: string) => {
-    setQuery(query);
+  const handleSearch = (newQuery: string) => {
+    setQuery(newQuery);
     setPage(1);
-    setMovies([]);
   };
 
-  useEffect(() => {
-    if (!query.trim()) return;
-    
-    const fetchData = async () => {
-      setIsLoading(true);
-      setIsError(false);
-      try {
-        const responseData = await fetchMovies(query, page);
-        setMovies(responseData.results);
-        setTotalPages(responseData.total_pages);
-      } catch {
-        setIsError(true);
-      }
-      finally {
-        setIsLoading(false);
-      }
-    };
-    fetchData();
-
-  }, [query, page]);
-
+  const movies = data?.results || [];
+  const totalPages = data?.total_pages || 0;
 
   return (
     <>
@@ -75,18 +63,18 @@ function App() {
       {selectedMovie !== null && (<MovieModal movie={selectedMovie} onClose={closeModal} />)}
       <Toaster position="top-center" reverseOrder={false} />
       {totalPages > 1 && !isLoading && !isError && (
-      <ReactPaginate
-        pageCount={totalPages}
-        pageRangeDisplayed={5}
-        marginPagesDisplayed={1}
-        onPageChange={({ selected }) => setPage(selected + 1)}
-        forcePage={page - 1}
-        containerClassName={css.pagination}
-        activeClassName={css.active}
-        nextLabel="→"
-        previousLabel="←"
-      />
-    )}
+        <ReactPaginate
+          pageCount={totalPages}
+          pageRangeDisplayed={5}
+          marginPagesDisplayed={1}
+          onPageChange={({ selected }) => setPage(selected + 1)}
+          forcePage={page - 1}
+          containerClassName={css.pagination}
+          activeClassName={css.active}
+          nextLabel="→"
+          previousLabel="←"
+        />
+      )}
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './components/App/App.tsx'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const element = document.getElementById('root');
+
+if (element !== null) {
+  const queryClient = new QueryClient();
+  
+  createRoot(element).render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </StrictMode>
+  );
+}
