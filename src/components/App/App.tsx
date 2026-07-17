d73cca50fb878c0739bd4cfb91290231d9f239b6
Taras <taras.zhovanyk@icloud.com>
2026-07-17 17:57:11 +0200

    First try to correction

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 
 import './App.css'
-import './App.module.css'
-import { useState } from 'react'
+import css from './App.module.css'
+import { useState, useEffect } from 'react'
 import { Toaster } from 'react-hot-toast'
 import SearchBar from '../SearchBar/SearchBar'
 import MovieGrid from '../MovieGrid/MovieGrid'
@@ -10,12 +10,24 @@ import ErrorMessage from '../ErrorMessage/ErrorMessage'
 import MovieModal from '../MovieModal/MovieModal'
 import type { Movie } from '../../types/movie'
 import { fetchMovies } from '../../services/movieService'
+import ReactPaginateModule from "react-paginate";
+import type { ReactPaginateProps } from "react-paginate";
+import type { ComponentType } from "react";
+
+type ModuleWithDefault<T> = { default: T };
+
+const ReactPaginate = (
+  ReactPaginateModule as unknown as ModuleWithDefault<ComponentType<ReactPaginateProps>>
+).default;
 
 function App() {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isError, setIsError] = useState<boolean>(false);
   const [movies, setMovies] = useState<Movie[]>([]);
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [page, setPage] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const [query, setQuery] = useState<string>('');
 
   const handleSelectMovie = (movie: Movie) => {
     setSelectedMovie(movie);
@@ -26,19 +38,32 @@ function App() {
   };
 
 
-  const handleSearch = async (query: string) => {
-    setIsLoading(true);
-    setIsError(false);
-    try {
-      const fetchedMovies = await fetchMovies(query);
-      setMovies(fetchedMovies);
-    } catch (error) {
-      setIsError(true);
-      console.error('Error fetching movies:', error);
-    } finally {
-      setIsLoading(false);
-    }
+  const handleSearch = (query: string) => {
+    setQuery(query);
+    setPage(1);
+    setMovies([]);
   };
+
+  useEffect(() => {
+    if (!query.trim()) return;
+    
+    const fetchData = async () => {
+      setIsLoading(true);
+      setIsError(false);
+      try {
+        const responseData = await fetchMovies(query, page);
+        setMovies(responseData.results);
+        setTotalPages(responseData.total_pages);
+      } catch {
+        setIsError(true);
+      }
+      finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+
+  }, [query, page]);
 
 
   return (
@@ -48,7 +73,20 @@ function App() {
       {isError && <ErrorMessage />}
       {!isLoading && !isError && movies.length > 0 && (<MovieGrid movies={movies} onSelect={handleSelectMovie} />)}
       {selectedMovie !== null && (<MovieModal movie={selectedMovie} onClose={closeModal} />)}
-     <Toaster position="top-center" reverseOrder={false} />
+      <Toaster position="top-center" reverseOrder={false} />
+      {totalPages > 1 && !isLoading && !isError && (
+      <ReactPaginate
+        pageCount={totalPages}
+        pageRangeDisplayed={5}
+        marginPagesDisplayed={1}
+        onPageChange={({ selected }) => setPage(selected + 1)}
+        forcePage={page - 1}
+        containerClassName={css.pagination}
+        activeClassName={css.active}
+        nextLabel="→"
+        previousLabel="←"
+      />
+    )}
     </>
   )
 }
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -3,6 +3,7 @@ import type { Movie } from '../types/movie';
 
 interface FetchMoviesResponse {
   results: Movie[];
+  total_pages: number;
 }
 
 const options = {
@@ -11,10 +12,10 @@ const options = {
   }
 };
 
-export const fetchMovies = async (query: string): Promise<Movie[]> => {
-  const url = `https://api.themoviedb.org/3/search/movie?query=${query}`;
+export const fetchMovies = async (query: string, page: number): Promise<FetchMoviesResponse> => {
+  const url = `https://api.themoviedb.org/3/search/movie?query=${query}&page=${page}`;
 
-  const response = await axios.get<FetchMoviesResponse>(url, options);
+  const response = await axios.get <FetchMoviesResponse>(url, options);
   
-  return response.data.results;
+  return response.data;
 }
